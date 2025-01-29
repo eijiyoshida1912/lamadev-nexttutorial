789af794f76ed38557504139d8b52ceeeec6d6cd
eiji yoshida <eijiyoshida1912@gmail.com>
2025-01-29 22:25:01 +0900

    40:30

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./footer.module.css"
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>&copy; lama-dev</div>
       <div></div>
     </div>
