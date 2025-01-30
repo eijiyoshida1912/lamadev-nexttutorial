0fe82b6b49bbade6ea95e8207b74d61ba83ba9d1
eiji yoshida <eijiyoshida1912@gmail.com>
2025-01-30 14:48:30 +0900

    47:47

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./footer.module.css"
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>&copy; lama-dev</div>
-      <div></div>
+      <div>
+        <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+        </div>
+      </div>
     </div>
   );
 };
